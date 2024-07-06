97d40158e7162f64e983d2d458f6cdecb5050183
Swathijanke <Swathijanke@accumenta.co.in>
2024-07-06 18:04:51 +0530

    forgot page changes

--- src/components/Dashboard/PageContainer.js
+++ src/components/Dashboard/PageContainer.js
@@ -1,14 +1,19 @@
 import React from 'react'
 import ProfileList from './ProfileList'
 import './Dashboard.scss'
+import Filters from './filters'
 const PageContainer = props => {
   return (
-    <div className='dashbcg'>
+    <div className='dashbcg mt-4'>
       <div className='d-flex justify-content-center'>
-        {/* <div className='w-25'>testing jgfjhdsgfjgsjdfjsgdfjgsj</div> */}
-        <div className='d-flex flex-column'>
+        <div className='w-50'></div>
+        <div className='d-flex flex-column mt-4'>
+          <div className="mt-4">
+          <Filters/>
           <div> Welcome to YUKTHI!</div>
           <ProfileList></ProfileList>
+          </div>
+
         </div>
       </div>
     </div>
