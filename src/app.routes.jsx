cead89be803c6eef1331fbb281afea36f217ace6
accumentamatrimonyproject <accumenta03@gmail.com>
2024-07-08 18:31:42 +0530

    MatchingProfile styling and functionalities are  completed

--- src/app.routes.jsx
+++ src/app.routes.jsx
@@ -5,6 +5,7 @@ import ForgotPage from './components/Forgot/ForgotPage'
 import SignUp from './components/Signup/SignUp'
 import Dashboard from './components/Dashboard/Dashboard'
 import { Redirect } from 'react-router-dom/cjs/react-router-dom'
+import Userprofile1 from './components/UserProfile/Userprofile1'
 
 const Routes = () => {
     return (
@@ -21,6 +22,9 @@ const Routes = () => {
                 </Route>
                 <Route path="/dashboard">
                     <Dashboard/>
+                </Route>
+                <Route path="/userprofile">
+                    <Userprofile1/>
                 </Route>
                 <Redirect path='/' to="login"></Redirect>
             </Switch>
