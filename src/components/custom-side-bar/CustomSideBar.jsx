0042b54b68c657e393b41262c9bd9b9d7f6a0abc
unknown <shashidharsunnapu@accumenta.co.in>
2024-07-10 21:12:09 -0700

    delete alert functionality completed

--- src/components/custom-side-bar/CustomSideBar.jsx
+++ src/components/custom-side-bar/CustomSideBar.jsx
@@ -5,13 +5,13 @@ import { RxCross2 } from "react-icons/rx";
 import { MdEdit } from "react-icons/md";
 
 import 'bootstrap/dist/css/bootstrap.min.css';
-import './CustomSideBar.css';
+import './CustomSideBar.scss';
 import Settings from '../settings/Settings';
 
 const CustomSideBar = () => {
   const [activeContent, setActiveContent] = useState('userImage');
   const [isSidebarOpen, setIsSidebarOpen] = useState(false);
-
+ 
   const buttonData = [
     { id: 'editImage', label: 'Edit Image' },
     { id: 'addPreferences', label: 'Add Preferences' },
@@ -22,7 +22,7 @@ const CustomSideBar = () => {
   ];
 
   const RenderContent = () => {
-    //replace all the items with component which we going to develop
+    // Replace all the items with component which we going to develop
     switch (activeContent) {
       case 'editImage':
         return <div>Edit Image Content</div>;
@@ -42,41 +42,37 @@ const CustomSideBar = () => {
   };
 
   return (
-    <>
-      <Container fluid className='outer-container'>
-        <Row className='row'>
-          <Col xs="11" className="d-flex flex-row justify-content-between d-md-none">
-            <button className="toggle-button" onClick={() => setIsSidebarOpen(!isSidebarOpen)}>
-            {isSidebarOpen?
-            <RxCross2 size={60} color='#780024'/>:<FaBarsStaggered size={60} color='#780024'/>}
-            </button>
-            <div className='image-container'>
+    <Container fluid className='outer-container'>
+      <Row className='row'>
+        <Col xs="12" className="d-flex flex-row justify-content-between d-md-none">
+          <Button className="bg-transparent border-0" onClick={() => setIsSidebarOpen(!isSidebarOpen)}>
+            {isSidebarOpen ? <RxCross2 size={30} color='#780024' /> : <FaBarsStaggered size={30} color='#780024' />}
+          </Button>
+          <div className='image-container'>
                <MdEdit className='add'/>
-            </div>
-          </Col>
-          <Col xs="12" md="3" className={`sidebar ${isSidebarOpen ? 'open' : ''}`}>
-            <div className='image-container1'>
-              <Button className='d-flex align-item-center justify-content-center add'>
-                Add
-               <MdEdit size={'16px'} className='mt-1'/>
+          </div>
+        </Col>
+        <Col xs="12" md="3" className={`sidebar ${isSidebarOpen ? 'open' : ''}`}>
+          <div className='image-container1'>
+            <Button className='d-flex align-item-center justify-content-center border-0 position-absolute bottom-0 add'>
+              Add
+               <MdEdit size={'10px'} className='mt-1'/>
+            </Button>
+          </div>
+          {buttonData.map((button, index) => (
+            <div key={button.id} className='button-parent'>
+              <Button block="true" className="bg-transparent border-0 text-warning" onClick={() => setActiveContent(button.id)}>
+                {button.label}
               </Button>
+              {index !== buttonData.length - 1 && <hr className='hr' />}
             </div>
-            {buttonData.map((button, index) => (
-              <div key={button.id} className='button-parent'>
-                <button block="true" className="sidebar-button" onClick={() => setActiveContent(button.id)}>
-                  {button.label}
-                </button>
-                {index !== buttonData.length - 1 && <hr className='hr' />}
-              </div>
-            ))}
-          </Col>
+          ))}
+        </Col>
           <Col xs="12" md="8"  data-testid="content">
-            <RenderContent />
-          </Col>
-        </Row>
-      </Container>
- 
-    </>
+          <RenderContent />
+        </Col>
+      </Row>
+    </Container>
   );
 };
 
