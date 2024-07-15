b4ecf368542d713f66fe1c4c26374b2cf01ec995
Swathijanke <Swathijanke@accumenta.co.in>
2024-07-15 21:11:06 +0530

    filter changes

--- src/components/Dashboard/ProfileList.jsx
+++ src/components/Dashboard/ProfileList.jsx
@@ -7,21 +7,46 @@ import { CiHeart } from "react-icons/ci";
 import Filters from './filters';
 
 const ProfileList = () => {
-  const [filterdata, setfilterData]=useState(testjson)
+  const [filterdata, setfilterData]=useState()
    const handleFilters=(data)=>{
     
+   }
+   useEffect(()=>{
+    setfilterData(testjson)
+   },[testjson])
+   const filters=(data,maindata)=>{
+let updatedfilteredData=[]
+    if(data.religion !=='' && data.subcast==='' && data.cast ===''){
+      updatedfilteredData= maindata.filter(item => item.religion.toLowerCase().includes(data.religion.toLowerCase()))
+    }  else if(data.religion==='' && data.subcast !==''&& data.cast ===''){
+      updatedfilteredData= maindata.filter(item => item.subcast.toLowerCase().includes(data.subcast.toLowerCase()))
+    }else if(data.religion ==='' && data.subcast === ''&& data.cast !==''){
+      updatedfilteredData= maindata.filter(item => item.cast.toLowerCase().includes(data.cast.toLowerCase()))
+    }else if(data.religion !=='' && data.subcast === '' && data.cast !==''){
+      updatedfilteredData= maindata.filter(item => item.religion.toLowerCase().includes(data.religion.toLowerCase()) && item.cast.toLowerCase().includes(data.cast.toLowerCase()))
+    }else if(data.religion !=='' && data.subcast !== '' && data.cast ===''){
+      updatedfilteredData= maindata.filter(item => item.religion.toLowerCase().includes(data.religion.toLowerCase()) && item.subcast.toLowerCase().includes(data.subcast.toLowerCase()))
+    } else if(data.religion ==='' && data.subcast !== '' && data.cast !==''){
+      updatedfilteredData= maindata.filter(item => item.cast.toLowerCase().includes(data.cast.toLowerCase()) && item.subcast.toLowerCase().includes(data.subcast.toLowerCase()))
+    }else if(data.religion !=='' && data.cast !=='' && data.subcast !==''){
+      updatedfilteredData= maindata.filter(item => item.religion.toLowerCase().includes(data.religion.toLowerCase()) && item.cast.toLowerCase().includes(data.cast.toLowerCase())&& item.subcast.toLowerCase().includes(data.subcast.toLowerCase()))
+    }
+    return updatedfilteredData
+   }
+   const handleBasic=(data)=>{
+    setfilterData(filters(data,testjson))
    }
 
   return (
     <div>
-      <Filters handlefilters={handleFilters}/>
+      <Filters handlefilters={handleFilters} handleBasic={handleBasic}/>
       <div className="row d-flex justify-content-center align-items-center w-100 bcg">
 
-    {filterdata.map(val=>(
+    {filterdata?.map(val=>(
          <div className="col-md-3 mr-2 cardwidth">
         <Card
         body
-        className={val.color==='red'?"my-2 cardColorredDash":"my-2 cardColorDash"}
+        className={val?.color==='red'?"my-2 cardColorredDash":"my-2 cardColorDash"}
       >
         <div className='d-flex justify-content-between w-100'>
           <CiSquareMinus color='white'size={25} />
