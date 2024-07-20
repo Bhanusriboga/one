d1d7fd9564937f07c224404012f784ffab6f327a
kishore <KishoreG@accumenta.co.in>
2024-07-20 10:24:41 +0000

    Updated index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -6,7 +6,6 @@ import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'react-toastify/dist/ReactToastify.css';
 import { ToastContainer } from 'react-toastify';
-import "./web.config";
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <>
