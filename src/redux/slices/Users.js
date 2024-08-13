af6fdad8f60e244519c243822712a6aaa0253e45
unknown <shashidharsunnapu@accumenta.co.in>
2024-08-13 11:43:57 +0530

    forapi updated as per the api change

--- src/redux/slices/Users.js
+++ src/redux/slices/Users.js
@@ -6,7 +6,7 @@ import Storage from '../../utils/Storage';
 export const getAllUsers = createAsyncThunk(
   "users/getAllUsers",
   async (_, thunkAPI) => {
-    const { response } = await networkCall(endPoints.getAllUsers, "GET");
+    const { response } = await networkCall(endPoints.getAllUsers+Storage.get("userId"), "GET");
     if (response) {
       return thunkAPI.fulfillWithValue(response);
     }
