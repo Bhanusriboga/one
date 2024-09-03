53ddbee96dfb70ada33c19e8260389d8847d5fba
unknown <shashidharsunnapu@accumenta.co.in>
2024-09-03 12:38:20 +0530

    terms and conditions

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React, { useEffect,useState } from 'react'
 import { useDispatch } from 'react-redux'
 import Header from './Header'
 import FooterBar from './FooterBar'
@@ -7,9 +7,16 @@ import { fetchUserInfo } from '../../redux/slices/AuthSlice'
 import './Dashboard.scss'
 import { getCaste } from '../../redux/slices/users'
 // import ComingSoon from '../ComingSoon/ComingSoon'
+import TermsAndConditions from "../Signup/TermsAndConditions.jsx";
+import { terms_conditios} from "../Signup/assets"
+import * as pdfWorker from 'pdfjs-dist/build/pdf.worker.mjs';
+
+import { pdfjs } from "react-pdf";
+pdfjs.GlobalWorkerOptions.workerSrc = URL.createObjectURL(new Blob([pdfWorker], { type: 'application/javascript' }));
 
 const Dashboard = () => {
   const dispatch=useDispatch()
+  const [termsCondition, setTermsCondition] = useState(false);
   const getUSerInfo=async()=>{
     await dispatch(fetchUserInfo())
     await dispatch(getCaste())
@@ -21,6 +28,9 @@ const Dashboard = () => {
   const scrollToBottom=()=>{
     botRef?.current?.scrollIntoView({behavior:'smooth'})
   }
+  const setTrueTermAndConditions=()=>{
+    setTermsCondition(true)
+  }
   return (
     <div className='h-100 w-100 pgback'>
       <div className='pageHeader'>
@@ -28,9 +38,16 @@ const Dashboard = () => {
       </div>
       <div className='pageContainer conwidth'>
         {/* <ComingSoon/> */}
+        {termsCondition && (
+        <div className="terms-condition-parent overlay " onClick={() => setTermsCondition(false)}>
+          <div className="terms-condition">
+            <TermsAndConditions pdfUrl={terms_conditios} />
+          </div>
+        </div>
+      )}
         <PageContainer/>
         <div className='pageFooter' ref={botRef}>
-        <FooterBar/>
+        <FooterBar setTermsCondition={setTrueTermAndConditions} />
       </div>
       </div>
 
